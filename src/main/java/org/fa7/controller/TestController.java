ceab29305ee2e85c84fafe628d7515a49dc814d5
Esdras Barreto <esdras.barreto@gmail.com>
2016-10-19 00:11:56 -0300

    Added Hikari CP

--- src/main/java/org/fa7/controller/TestController.java
+++ src/main/java/org/fa7/controller/TestController.java
@@ -1,13 +1,19 @@
 package org.fa7.controller;
 
+import org.fa7.service.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private ReservaService reservaService;
+
     @RequestMapping("/ping")
     public String ping() {
+        reservaService.validate();
         return "pong";
     }
 }
